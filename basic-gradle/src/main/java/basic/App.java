6bb596cffd97610815a84f28b5324e9701a4961e
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2019-05-30 14:29:09 +0100

    add dependency to gradle java project

--- basic-gradle/src/main/java/basic/App.java
+++ basic-gradle/src/main/java/basic/App.java
@@ -1,5 +1,8 @@
 package basic;
 
+import java.util.Date;
+import org.ocpsoft.prettytime.PrettyTime;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
@@ -10,5 +13,9 @@ public class App {
         separator.print('*', 30);
         
         System.out.println(new App().getGreeting());
+
+        separator.print('*', 30);
+        PrettyTime prettyTime = new PrettyTime(new Date(0));
+        System.out.println("This is happening: " + prettyTime.format(new Date(1000 * 60 * 60 * 24)));
     }
 }
